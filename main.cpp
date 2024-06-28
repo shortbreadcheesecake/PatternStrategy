b530b1c071ec7e374acddc7f4617481e3c5e2346
shortbreadcheesecake <119790393+shortbreadcheesecake@users.noreply.github.com>
2024-06-28 21:29:19 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include<pattern.h>
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+    Cat cat;
+    Dog dog;
+    FoodDispenser dispenser;
+
+    cout << "Кормим кошку:" << endl;
+    dispenser.feed(&cat);
+    cout << "Кошка сыта" << endl;
+
+    cout << "\nКормим собаку:" << endl;
+    dispenser.feed(&dog);
+    cout << "Собака сыта" << endl;
+
+    return 0;
+}
---
+++ pattern.h
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+    virtual void eat() = 0;
+};
+class Dog : public Animal {
+public:
+    void eat() override {
+        cout << "Собака съела мясо и кости" << endl;
+    }
+};
+class Cat : public Animal {
+public:
+    void eat() override {
+        cout << "Кошка съела рыбу и молоко" << endl;
+    }
+};
+class FoodDispenser {
+public:
+    void feed(Animal* animal) {
+        animal->eat();
+    }
+};
